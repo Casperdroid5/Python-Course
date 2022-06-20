dfdcccb0bd2dc5771ff00ecadec0aed3c6bfd1d6
Casperdroid5 <caspertak1@gmail.com>
2022-06-20 23:06:25 +0200

    started python course

---
+++ Giraffe/app.py
@@ -0,0 +1,15 @@
+
+#tuples: a container wherein we can store different values.
+#key differences from list: tuples not mutable, cannot be changed,removed or anything, what you see is what you get.
+#coordinates[1] = 1 <- cannot be done, since tuples values cannot be changed ever
+#tuples is for data that is never going to change
+"""
+coordinates = (4, 5)
+print(coordinates[0]) #prints out index x
+
+""" #dissables previous exercises
+#ex12 Functions
+#functions:
+
+
+
