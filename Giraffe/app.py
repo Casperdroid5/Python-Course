c999c89cf529b73f42d8ad740d6da2ce38d69e7a
Casperdroid5 <caspertak1@gmail.com>
2022-06-27 17:08:52 +0200

    finished till exercise 31

--- Giraffe/app.py
+++ Giraffe/app.py
@@ -375,5 +375,30 @@ print(student1.gpa)
 print(student2.name)
 print(student2.gpa)
 print("\n")
-"""  # end of disabled code
+
 #ex30 Building a Multiple Choice Quiz
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) purple\n(c) Orange\n\n",
+    "What color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n",
+]
+from Question import Question
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct!")
+
+run_test(questions)  # Run the function
+print("\n")
+"""  # end of disabled code
+
+#ex31 Object Functions
